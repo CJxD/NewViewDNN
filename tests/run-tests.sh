fc07e721f1c51954047284eb2176e9c21b4d0d2a
Chris Watts <cwatts1@us.ibm.com>
2017-06-06 13:59:30 +0100

    Test updates

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
 
-data_path="../data"
+script="../run-test.sh"
 
-script="../src/models/run-autoencoder.py"
-flags=""
-train_flags="train -i $data_path/train-nc.tfrecords --summary-interval 500 $flags"
-val_flags="validate -i $data_path/val-nc.tfrecords --summary-interval 0 $flags"
-run_flags="run -i $data_path/test_single.txt --summary-interval 0 $flags"
+$script -x 32 -y 32 -C 3 --model-file checkpoints/32x32x3/model.ckpt -o outputs/32x32x3 | tee 32x32x3.log
 
-$script $train_flags -x 32 -y 32 -C 3 --model-file checkpoints/32x32x3/model.ckpt --log-dir logs/32x32x3
-$script $val_flags -x 32 -y 32 -C 3 --model-file checkpoints/32x32x3/model.ckpt | tee 32x32x3-validation.log
-$script $run_flags -x 32 -y 32 -C 3 --model-file checkpoints/32x32x3/model.ckpt -o outputs/32x32x3
+$script -x 32 -y 32 -C 3 --filter-sizes 7 7 7 --model-file checkpoints/32x32x3-777/model.ckpt -o outputs/32x32x3-777 | tee 32x32x3-777.log
 
-$script $train_flags -x 32 -y 32 -C 3 -d --model-file checkpoints/32x32x3-diff/model.ckpt --log-dir logs/32x32x3-diff
-$script $val_flags -x 32 -y 32 -C 3 -d --model-file checkpoints/32x32x3-diff/model.ckpt | tee 32x32x3-diff-validation.log
-$script $run_flags -x 32 -y 32 -C 3 -d --model-file checkpoints/32x32x3-diff/model.ckpt -o outputs/32x32x3-d
+$script -x 32 -y 32 -C 3 --filter-sizes 3 3 3 3 3 --num-filters 10 10 10 10 10 --model-file checkpoints/32x32x3-5deep/model.ckpt -o outputs/32x32x3-5deep | tee 32x32x3-5deep.log
 
-$script $train_flags -x 32 -y 32 -C 4 --model-file checkpoints/32x32x4/model.ckpt --log-dir logs/32x32x4
-$script $val_flags -x 32 -y 32 -C 4 --model-file checkpoints/32x32x4/model.ckpt | tee 32x32x4-validation.log
-$script $run_flags -x 32 -y 32 -C 4 --model-file checkpoints/32x32x4/model.ckpt -o outputs/32x32x4
+$script -x 32 -y 32 -C 3 -d --model-file checkpoints/32x32x3-diff/model.ckpt -o outputs/32x32x3-diff | tee 32x32x3-diff.log
 
-$script $train_flags -x 64 -y 64 -C 3 --model-file checkpoints/64x64x3/model.ckpt --log-dir logs/64x64x3
-$script $val_flags -x 64 -y 64 -C 3 --model-file checkpoints/64x64x3/model.ckpt | tee 64x64x3-validation.log
-$script $run_flags -x 64 -y 64 -C 3 --model-file checkpoints/64x64x3/model.ckpt -o outputs/64x64x3
+$script -x 32 -y 32 -C 4 --model-file checkpoints/32x32x4/model.ckpt -o outputs/32x32x4 | tee 32x32x4.log
 
-$script $train_flags -x 128 -y 128 -C 3 --model-file checkpoints/128x128x3/model.ckpt --log-dir logs/128x128x3
-$script $val_flags -x 128 -y 128 -C 3 --model-file checkpoints/128x128x3/model.ckpt | tee 128x128x3-validation.log
-$script $run_flags -x 128 -y 128 -C 3 --model-file checkpoints/128x128x3/model.ckpt -o outputs/128x128x3
+$script -x 64 -y 64 -C 3 --model-file checkpoints/64x64x3/model.ckpt -o outputs/64x64x3 | tee 64x64x3.log
 
-$script $train_flags -x 256 -y 256 -C 3 --model-file checkpoints/256x256x3/model.ckpt --log-dir logs/256x256x3
-$script $val_flags -x 256 -y 256 -C 3 --model-file checkpoints/256x256x3/model.ckpt | tee 256x256x3-validation.log
-$script $val_flags -x 256 -y 256 -C 3 --model-file checkpoints/256x256x3/model.ckpt -o outputs/256x256x3
+$script -x 128 -y 128 -C 3 --model-file checkpoints/128x128x3/model.ckpt -o outputs/128x128x3 | tee 128x128x3.log
 
-$script $train_flags -X 512 -Y 512 -C 3 --model-file checkpoints/512x512x3/model.ckpt --log-dir logs/512x512x3
-$script $val_flags -X 512 -Y 512 -C 3 --model-file checkpoints/512x512x3/model.ckpt | tee 512x512x3-validation.log
-$script $run_flags -X 512 -Y 512 -C 3 --model-file checkpoints/512x512x3/model.ckpt -o outputs/512x512x3
+$script -x 256 -y 256 -C 3 --model-file checkpoints/256x256x3/model.ckpt -o outputs/256x256x3 | tee 256x256x3.log
+
+$script -X 512 -Y 512 -x 512 -y 512 -C 3 --model-file checkpoints/512x512x3/model.ckpt -o outputs/512x512x3 | tee 512x512x3.log
