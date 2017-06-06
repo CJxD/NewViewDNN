3724d7d3d6a5d1da3209e9d8a3d3cf2a25f5c494
CJxD <chris@cjwatts.com>
2017-06-06 16:34:41 +0000

    Run all tests in difference mode

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -3,7 +3,7 @@
 data_path="../data"
 
 script="../src/models/run-autoencoder.py"
-flags="$*"
+flags="$* -d"
 train_flags="train -i $data_path/train-nc.tfrecords --summary-interval 500 $flags"
 val_flags="validate -i $data_path/val-nc.tfrecords --summary-interval 0 $flags"
 run_flags="run -i $data_path/test_single.txt --summary-interval 0 $flags"
