b8bf815df3ffb81f0a5d4f6bdc22780801117194
CJxD <chris@cjwatts.com>
2017-06-11 11:05:32 +0000

    Unescape training and validating

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -9,9 +9,9 @@ val_flags="validate -i $data_path/val-nc.tfrecords --summary-interval 0 $flags"
 run_flags="run -i $data_path/test_single.txt --summary-interval 0 $flags"
 
 echo "========== Training ========="
-#$script $train_flags
+$script $train_flags
 echo "========= Validating ========"
-#$script $val_flags
+$script $val_flags
 echo "========== Testing =========="
 $script $run_flags
 
